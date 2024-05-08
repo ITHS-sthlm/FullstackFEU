39a71634001928b841bcb8d288ee60a8c67206ef
Navigoo <...>
2024-05-08 11:06:46 +0200

    with sql

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,34 @@
 const express = require('express'),
-path = require('path')
+    path = require('path');
+const sqlite3 = require('sqlite3').verbose();
 
-const app = express()
+const app = express();
 
-app.get('/api/contact', (_request, response) => {
+//Öppna anslutning till DB
+const db = new sqlite3.Database(path.resolve(__dirname, 'test.sqlite'));
+
+/* app.get('/api/contact', (_request, response) => {
     response.send({ hello: "World"})
-})
+}) */
 
-app.use(express.static(path.join(path.resolve(), 'dist')))
+//Endpoint för att hämta all data från databasen
+app.get('/cities', (_req, res) => {
+    db.all('SELECT * FROM cities', (err, rows) => {
+        if (err) {
+            console.error(err.message);
+            res.status(500).send('Database error');
+            return;
+        }
+        res.json(rows);
+    });
+});
 
-app.listen(3000, () => {
-    console.log('Redo på http://localhost:3000')
-})
+//Nedför lägger ni in flera endpoints 
+
+//Middleware för att läsa statiska filer med express från mappen dist
+app.use(express.static(path.join(path.resolve(), 'dist')));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log('Redo på http://localhost:3000');
+});
