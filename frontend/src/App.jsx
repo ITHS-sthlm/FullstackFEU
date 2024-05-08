a21c04d4f10da5a4f7fd3c17d756067b825e16bd
Navigoo <...>
2024-05-08 11:54:47 +0200

    2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import './App.css';
+import Hello from './components/Hello';
 
 function App() {
     const [cities, setCities] = useState([]);
@@ -20,6 +21,7 @@ function App() {
                     </li>
                 ))}
             </ul>
+            <Hello/>
         </div>
     );
 }
